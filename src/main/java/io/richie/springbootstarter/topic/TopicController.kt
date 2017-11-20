72b8c8a35388ab69c67b15087b670e7f73af43da
Richard Walker <richard.walker.90@gmail.com>
2017-11-19 21:34:09 -0500

    moved topic package underneath starter package

--- src/main/java/io/richie/springbootstarter/topic/TopicController.kt
+++ src/main/java/io/richie/springbootstarter/topic/TopicController.kt
@@ -1,4 +1,4 @@
-package io.richie.topic
+package io.richie.springbootstarter.topic
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
